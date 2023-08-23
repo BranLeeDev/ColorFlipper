175904834d741774c6643964561bb035264ce43d
Brandon <name.brandon.aguero@gmail.com>
2023-08-23 11:25:47 -0500

    Codigo que proporciona colores hexadecimales aleatorios y agregas clases para evitar que el color del button y paragraph sean iguales al background de content-layoutd

---
+++ js/hex.js
@@ -0,0 +1,25 @@
+const sectionButton = document.querySelector(".section__button");
+const sectionParagraph = document.querySelector(".section__paragraph");
+const paragraphColor = document.querySelector(".paragraph__color");
+const contentLayout = document.querySelector(".content-layout");
+const hexOptions = "0123456789ABCDEF";
+
+sectionButton.addEventListener("click", () => {
+  let colorHex = "#";
+
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * hexOptions.length);
+    colorHex += hexOptions[randomIndex];
+  }
+
+  if (colorHex === "#222222") {
+    sectionButton.classList.add("equals-button");
+    sectionParagraph.classList.add("equals-paragraph");
+  } else {
+    sectionParagraph.classList.remove("equals-paragraph");
+    sectionButton.classList.remove("equals-button");
+  }
+
+  paragraphColor.textContent = colorHex;
+  contentLayout.style.backgroundColor = colorHex;
+});
