0aef9256f733fe419043a22db3d27373ed60c5a5
Brandon <name.brandon.aguero@gmail.com>
2023-08-24 09:26:28 -0500

    Código javascript principal, este forma un X

--- js/main.js
+++ js/main.js
@@ -1,31 +1,8 @@
-const sectionButton = document.querySelector(".section__button");
-const sectionParagraph = document.querySelector(".section__paragraph");
-const paragraphColor = document.querySelector(".paragraph__color");
-const contentLayout = document.querySelector(".content-layout");
-const hexOptions = "0123456789";
+const navButton = document.querySelector(".nav__button");
+const buttonLine = document.querySelectorAll(".button__line");
 
-sectionButton.addEventListener("click", () => {
-  let result = "";
-  let numberRed;
-  let numberGreen;
-  let numberBlue;
-  for (let i = 0; i <= 2; i++) {
-    const randomIndex = Math.floor(Math.random() * 256);
-    if (i === 0) numberRed = randomIndex;
-    if (i === 1) numberGreen = randomIndex;
-    if (i === 2) numberBlue = randomIndex;
-  }
-
-  if (result === "rgb(33, 33, 33)") {
-    sectionButton.classList.add("equals-button");
-    sectionParagraph.classList.add("equals-paragraph");
-  } else {
-    sectionParagraph.classList.remove("equals-paragraph");
-    sectionButton.classList.remove("equals-button");
-  }
-
-  result = `rgb(${numberRed}, ${numberGreen}, ${numberBlue})`;
-
-  paragraphColor.textContent = result;
-  contentLayout.style.backgroundColor = result;
+navButton.addEventListener("click", () => {
+  buttonLine[0].classList.toggle("button__line--one");
+  buttonLine[1].classList.toggle("button__line--two");
+  buttonLine[2].classList.toggle("button__line--three");
 });
