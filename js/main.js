50b91bb6e972aa3b086e8e7d75ba720926aba9db
Brandon <name.brandon.aguero@gmail.com>
2023-08-24 10:24:03 -0500

    Ahora hay un slide funcional, cada vez que se presiona el menu-hamburgesa este genera un slide con una transicion suave

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const navButton = document.querySelector(".nav__button");
 const buttonLine = document.querySelectorAll(".button__line");
+const navList = document.querySelector(".nav__list");
 
 navButton.addEventListener("click", () => {
   buttonLine[0].classList.toggle("button__line--one");
   buttonLine[1].classList.toggle("button__line--two");
   buttonLine[2].classList.toggle("button__line--three");
+
+  navList.classList.toggle("nav__list--active");
 });
